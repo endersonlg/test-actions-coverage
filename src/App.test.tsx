f56d78f0201c6c156b21a0217a416d12f3ffdaf6
endersonlg <endersonluciog@gmail.com>
2021-08-13 13:43:46 -0300

    up

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import { render } from '@testing-library/react';
+
 import App from './App';
 
 test('renders learn react link', () => {
-  const {getByText} = render(<App />);
+  const { getByText } = render(<App />);
   const linkElement = getByText('Test Git Hub Actions');
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+// eslint-disable-next-line no-use-before-define
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Test Git Hub Actions</h1>
-      <p>ola</p>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className="App">
+    <h1>Test Git Hub Actions</h1>
+    <p>ola</p>
+  </div>
+);
 
 export default App;
