49c53e3eb9ae7ce22f6a64b151636a796cd3682d
endersonlg <endersonluciog@gmail.com>
2021-08-13 15:03:08 -0300

    up

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,6 +6,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText('Test Git Hub Actions');
+  const linkElement = getByText('Test Git Hub Actions 2');
   expect(linkElement).toBeInTheDocument();
 });
