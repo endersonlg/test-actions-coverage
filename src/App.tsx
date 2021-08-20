79b0d0fa4780c7f272a94344b49b0088c10de064
endersonlg <endersonluciog@gmail.com>
2021-08-20 15:27:40 -0300

    dadsadsa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,93 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Test Git Hub Actions 2</h1>
-    <p>ola teste 8</p>
-  </div>
-);
+const App: React.FC = () => {
+  const teste = () => {
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+    console.log('ola');
+  };
+  return (
+    <div className="App">
+      <h1>Test Git Hub Actions 2</h1>
+      <p>ola teste 8</p>
+      {false && (
+        <div>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+          <h1>Este conteudo nao ira renderizar para teste</h1>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
