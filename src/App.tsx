70dd41aea0e38ecb0fac5ee174a1c098ab2bd267
endersonlg <endersonluciog@gmail.com>
2021-08-24 10:07:40 -0300

    casa

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const App: React.FC = () => (
   <div className="App">
     <h1>Test Git Hub Actions 2</h1>
-    <p>ola teste 8</p>
+    <p>ola teste 8 la casa</p>
     {false && (
       <div>
         <h1>Este conteudo nao ira renderizar para teste</h1>
